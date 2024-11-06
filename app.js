ebf42ea753dce96e3260535e629ad6db3ca044e4
ikumisato <kinoko7971@gmail.com>
2024-11-06 09:38:06 +0900

    タイムラインを整理

--- app.js
+++ app.js
@@ -1,75 +1,47 @@
-const box1 = document.querySelector(".js-box:nth-child(1)");
-const box2 = document.querySelector(".js-box:nth-child(2)");
-const box3 = document.querySelector(".js-box:nth-child(3)");
+const box1 = document.querySelectorAll(".js-box1");
+const box2 = document.querySelectorAll(".js-box2");
+// const box2 = document.querySelector(".js-box:nth-child(2)");
+// const box3 = document.querySelector(".js-box:nth-child(3)");
 const textAll = document.querySelectorAll(".js-text"); // h1とpタグにjs-textのクラスを付与して下さい。
 const logo = document.querySelectorAll(".js-logo");
 
 // 4. timeline
 const TL = gsap.timeline({
-  defaults: {
-    autoAlpha: 0,
-    y: 30,
+  default: {
+    ease: "power2",
   },
 });
 
-TL.from(box1, { autoAlpha: 0 })
-  .from(box2, { autoAlpha: 0 })
-  .from(textAll, { autoAlpha: 1 })
-  .from(logo, { autoAlpha: 1 });
-
-// box3にクリックイベントを追加してアニメーションを実行する
-box3.addEventListener("click", () => {
-  gsap.to(box3, {
-    autoAlpha: 0,
-    duration: 2,
-  });
-});
-
-// 回転
-gsap.to(box2, {
-  x: 100,
-  rotate: 360,
-  scale: 0.5,
-  duration: 2,
-});
-// 位置移動　回転
-gsap.fromTo(
-  box1,
-  {
-    x: 0,
-    opacity: 0,
-    duration: 0.8,
-  },
-  {
-    x: 100,
-    opacity: 1,
-    rotation: 360,
-    duration: 2,
-  }
-);
-
-// 1. stagger
-gsap.from(textAll, {
-  autoAlpha: 0,
-  y: 50,
-  // x: "random(-100, 100. 5)", //このような書き方をする事で値をランダムにする事ができます。
-  // x: "random([-150, -100, 100, 150])",
+TL.to(box1, {
+  height: 200,
   duration: 0.8,
-  stagger: {
-    each: 0.8, //各アニメーションの間隔を指定します。
-    from: "random", //最初のアニメーションの間隔をランダムにします。
-  },
-});
-
-// stagger で出現
-gsap.from(logo, {
-  y: 10,
-  alpha: 0,
-  duration: 3,
-  ease: "bounce.out",
   stagger: {
     each: 0.08, // ばらす間隔（秒）
     from: "random", // ランダムに開始
   },
-  // stagger: 0.05, // 0.02秒ごとに出現
 });
+TL.to(
+  box2,
+  {
+    height: 200,
+    duration: 0.8,
+    stagger: {
+      each: 0.08, // ばらす間隔（秒）
+      from: "random", // ランダムに開始
+    },
+  },
+  "-=0.8"
+)
+  // .to(box2, { y: 0, height: 400, duration: 0.8 })
+  // .to(box3, { y: 0, duration: 0.8 })
+  .from(textAll, { autoAlpha: 0, duration: 0.8, delay: 0.7 })
+  .from(logo, {
+    y: 10,
+    alpha: 0,
+    duration: 2,
+    ease: "bounce.out",
+    stagger: {
+      each: 0.08, // ばらす間隔（秒）
+      from: "random", // ランダムに開始
+    },
+  });
